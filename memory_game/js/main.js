1f52b79b82607311f1395be1231a92f143c21055
Roxana Deng <roxanadeng@Roxanas-Air.fios-router.home>
2020-01-28 02:29:27 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,17 @@
-let cardOne = "King";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "Queen";
+
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
